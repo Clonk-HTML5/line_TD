53073425dfbcb86705695e0b5d704fb11caeb149
Christian Tobias Schmitt <ctt@christians-mbp.localdomain>
2014-09-14 00:10:49 +0200

    added tower select

--- game/prefabs/towerGroup.js
+++ game/prefabs/towerGroup.js
@@ -8,8 +8,11 @@ var TowerGroup = function(game, enemys) {
   // initialize your prefab here
 //    this.tower = new Tower(this.game, 13*GlobalGame.tileSquare, 15*GlobalGame.tileSquare, 3);
 //    this.add(this.tower);
+    this.towerFrameNumbers = [1,3,5,7];
+    this.currentTowerFrame = 3;
     this.tileForbiden = ["9", "10"];
     this.enemys = enemys;
+    this.showTowerImages();
     this.game.input.onDown.add(TowerGroup.prototype.posit, this);
     
     this.bullets = this.game.add.group();
@@ -67,7 +70,7 @@ TowerGroup.prototype.posit = function(pointer) {
             if(this.game.plugins.plugins[0] instanceof Phaser.Plugin.PathFinderPlugin){
                 this.game.plugins.plugins[0].avoidAdditionalPoint(tileX, tileY);
             }
-            this.tower = new Tower(this.game, tileworldX, tileworldY, 3, tileX, tileY, 'tower', this.bullets);
+            this.tower = new Tower(this.game, tileworldX, tileworldY, this.currentTowerFrame, tileX, tileY, 'tower', this.bullets);
             this.add(this.tower);
             this.towersBuilt++;
             this.maxTowersText.setText('Max Towers: ' + this.towersBuilt + ' / '+ this.maxTowers);
@@ -85,6 +88,19 @@ TowerGroup.prototype.posit = function(pointer) {
 TowerGroup.prototype.incomeHandler = function () {
     this.gold += this.income; 
     this.goldText.setText('Gold: ' + this.gold);
+}
+
+TowerGroup.prototype.showTowerImages = function () {
+    this.towerImageGroup = this.game.add.group(this.game, this, 'towerImageGroup')
+    for (var i = 0, len = this.towerFrameNumbers.length; i < len; i++){
+        this.towerImage = this.game.add.sprite(50*(i+1), this.game.height - 50, 'tower', this.towerFrameNumbers[i]);
+        this.towerImage.anchor.set(0.5);
+        this.towerImage.inputEnabled = true;
+        this.towerImageGroup.add(this.towerImage);
+    }
+    this.towerImageGroup.forEach(function(towerImage) {
+        towerImage.events.onInputDown.add(function(){this.currentTowerFrame = towerImage.frame;}, this);
+    }, this);
 }
 
 TowerGroup.prototype.bulletHitsEnemy = function (enemy, bullet) {
