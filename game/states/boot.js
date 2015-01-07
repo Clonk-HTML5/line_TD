3e4cbf0e3ab734b41de82a0596cc5783e6e43f24
System Administrator <root@Christians-MBP.localdomain>
2015-01-07 22:31:36 +0100

    scalemanagment

--- game/states/boot.js
+++ game/states/boot.js
@@ -97,17 +97,27 @@ Boot.prototype = {
     this.game.time.advancedTiming = true;
   },
   create: function() {
-    this.game.input.maxPointers = 1;
-    //scaling options
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-    
-    //have the game centered horizontally
-    this.scale.pageAlignHorizontally = true;
-    this.scale.pageAlignVertically = true;
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = true;
 
-    //screen size will be set automatically
-    this.scale.setScreenSize(true);
+        if (this.game.device.desktop)
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.setMinMax(480, 260, 800, 600);
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+        }
+        else
+        {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.setMinMax(480, 260, 800, 600);
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+            this.scale.forceOrientation(true, false);
+            this.scale.setResizeCallback(this.gameResized, this);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+        }
       
     this.game.state.start('preload');
   }
